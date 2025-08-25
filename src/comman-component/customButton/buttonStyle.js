89f08d3308d7186c7e3134f4b3348602721c9a25
aniket0806 <97946316+aniket0806@users.noreply.github.com>
2025-08-25 11:07:22 +0530

    Create a 404page

--- src/comman-component/customButton/buttonStyle.js
+++ src/comman-component/customButton/buttonStyle.js
@@ -1,8 +1,59 @@
 const buttonStyle = {
-    forgetButton: {
-        width: {xs: "100%",sm: "100%",md: "200px", lg: "240px" },
-        height: "48px"
-    }
-}
+  forgetButton: {
+    width: { xs: "100%", sm: "100%", md: "200px", lg: "240px" },
+    height: "48px",
+  },
+  notfoundButton: {
+    padding: {
+      xs: "12px 16px",
+      sm: "14px 18px",
+      md: "16px 20px",
+    },
+    backgroundColor: "#26B6E6",
+    color: "#FFFFFF",
+    fontSize: {
+      xs: "16px",
+      sm: "18px",
+      md: "20px",
+    },
+    fontWeight: 500,
+    borderRadius: "12px",
+  },
+  notfoundButton1: {
+    width: { xs: "100%", sm: "220px", md: "251px" },
+    padding: {
+      xs: "12px 16px",
+      sm: "14px 18px",
+      md: "16px 20px",
+    },
+    backgroundColor: "#1A2E33",
+    color: "#FFFFFF",
+    fontSize: {
+      xs: "16px",
+      sm: "18px",
+      md: "20px",
+    },
+    fontWeight: 500,
+    borderRadius: "12px",
+  },
+
+  notfoundButton2: {
+    width: { xs: "100%", sm: "170px", md: "198px" },
+    padding: {
+      xs: "12px 16px",
+      sm: "14px 18px",
+      md: "16px 20px",
+    },
+    backgroundColor: "#1A2E33",
+    color: "#FFFFFF",
+    fontSize: {
+      xs: "16px",
+      sm: "18px",
+      md: "20px",
+    },
+    fontWeight: 500,
+    borderRadius: "12px",
+  },
+};
 
 export default buttonStyle;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import CustomButton from "@/comman-component/customButton";
+import buttonStyle from "@/comman-component/customButton/buttonStyle";
+import CustomButton2 from "@/comman-component/customButton2";
+import Image from "next/image";
+import React from "react";
 
-const 404 = () => {
+const NotFound = () => {
   return (
-    <div>404</div>
-  )
-}
+    <div className="bg-[#DEF2FC]">
+      <div className=" custom-container py-20">
+    
+        <div className="flex flex-col items-center justify-center text-center">
+        <div className=" w-[738px] h-[214px] mt-20 relative">
+  <Image
+    src="/404image.png"
+    alt="404image"
+    fill
+    
+  />
+</div>
+          <div className="mt-10 dm_sans ">
+            <h1 className="responsive-heading text-[#1A2E33]">
+              <span className="font-bold ">{`Oops !`}</span>{" "}
+              {`Lost in the Trails?`}
+            </h1>
+            <h4 className="responsive-h4 text-[#4D5D60] font-medium ">
+              {`This page took a wrong turn and doesn’t exist.`}
+            </h4>
+            <h6 className="responsive-text mt-2 md:mt-5.5 text-[#4D5D60]">
+              {`But don’t worry — your next adventure is just a click away.`}
+            </h6>
+          </div>
+          <div className="flex flex-wrap justify-center sm:justify-normal gap-1 md:gap-2 sm:gap-2 mt-2.5">
+            <div className="manrope">
+              <CustomButton sx={buttonStyle.notfoundButton}>
+                Back to Home
+              </CustomButton>
+            </div>
+            <div>
+              <CustomButton2 sx={buttonStyle.notfoundButton1}>
+                Explore Tour Packages
+              </CustomButton2>
+            </div>
+            <div>
+              <CustomButton2 sx={buttonStyle.notfoundButton2}>
+                Contact Support
+              </CustomButton2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default 404
+export default NotFound;
